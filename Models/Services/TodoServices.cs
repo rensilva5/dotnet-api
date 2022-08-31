51f4364596d5e68f5753cb4559b18f312b9deabd
rensilva5 <renzoasilva@gmail.com>
2022-08-31 11:49:55 -0400

    Api using C#

---
+++ Models/Services/TodoServices.cs
@@ -0,0 +1,21 @@
+namespace Services; //create an interface below
+public interface ITodoServices{
+    List<Todo> GetAll();
+    Todo Create(string title);
+}
+
+public class TodoServices : ITodoServices
+{
+    private List<Todo> _todos = new List<Todo>();
+    public Todo Create(string title)
+    {
+        var todo = new Todo { Title = title, IsCompleted = false };
+        _todos.Add(todo);
+        return todo;
+     }
+
+     public List<Todo> GetAll()
+     {
+        return _todos;
+     }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,5 @@
+public class Todo
+{
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+}
